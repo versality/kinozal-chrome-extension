c5a21f8bbe453a3920051cb1346d85bed1c347bf
Artyom Pertsovsky <artyom@pertsovsky.com>
2014-09-20 18:46:25 +0300

    Add view for score and number of votes

--- src/popup.js
+++ src/popup.js
@@ -2,19 +2,20 @@ var table = $('table');
 
 chrome.storage.local.get('movies', function(data) {
   if (Object.keys(data).length === 0) return;
-  
+
   data['movies'].forEach(function(movie) {
-    if (!movie['seen'])
+    if (!movie.seen)
       $('table').append('\
         <tr>\
-          <td><a href='+ movie['url'] +'>'+movie['name']+'</a></td>\
+          <td><a href=' + movie.url + '>' + movie.name + '</a></td>\
+          <td>' + movie.rating.score + ' : ' + movie.rating.votes + '</td>\
           <td><input type="checkbox"></td>\
         </tr>');
   });
 
   if ($('table > tr').length === 0) {
     $('h3').show();
-  };
+  }
 
   $('a').on('click', function() {
     chrome.tabs.create({url: $(this).attr('href')});
@@ -26,9 +27,9 @@ chrome.storage.local.get('movies', function(data) {
     var movieName = $(this).parent().siblings().text();
 
     chrome.storage.local.get('movies', function(data) {
-      data['movies'].forEach(function(movie, index) {
-        if (movie['name'] === movieName)
-          data['movies'][index]['seen'] = true;
+      data.movies.forEach(function(movie, index) {
+        if (movie.name === movieName)
+          data.movies[index]['seen'] = true;
       });
 
       chrome.storage.local.set(data);
