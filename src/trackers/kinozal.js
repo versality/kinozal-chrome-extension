03be57254dc14c56fff541e3e0cca8ca79d512c5
Artyom Pertsovsky <artyom@pertsovsky.com>
2014-09-20 18:44:15 +0300

    Add IMDB support for kinozal

--- src/trackers/kinozal.js
+++ src/trackers/kinozal.js
@@ -1,39 +1,80 @@
-function kinozal(callback) {  
-  var movies = [];
-  var trs;
+function kinozal(callback) {
+    var movies = [];
+    var trs;
 
-  function parse_trs(data) {
-    trs = $(data).find('.w100p > tbody > tr');
+    $.get('http://kinozal.tv/browse.php?s=&g=0&c=0&v=0&d=0&w=0&t=1&f=0/', parse_trs);
 
-    trs.each(function() {
-      $(this).each(function () {  
-        var _time = $(this).children('.s:eq(2)').text();
+    function parse_trs(data) {
+        trs = $(data).find('.w100p > tbody > tr');
 
-        _time = _time.replace(/[\u0250-\ue007]/g, '');
-        _time = _time.split('  ');
+        trs.each(function() {
+            $(this).each(function () {
+                var _time = $(this).children('.s:eq(2)').text();
 
-        var timeDate = _time[0].split('.');
-        var time = _time[1];
+                _time = _time.replace(/[\u0250-\ue007]/g, '');
+                _time = _time.split('  ');
 
-        if (timeDate[0] === '') {
-          finalDate = new Date().getTime();
-        } else {
-          finalDate = Date.parse(timeDate[2]+"-"+timeDate[1]+"-"+timeDate[0]+"T"+time);
-        }
+                var timeDate = _time[0].split('.');
+                var time = _time[1];
 
-        movies.push({
-          'name': $(this).children('.nam').text(),
-          'time': finalDate,
-          'url': 'http://kinozal.tv' + $(this).children('.nam').children().attr('href'),
-          'seen': false
+                if (timeDate[0] === '') {
+                    finalDate = new Date().getTime();
+                } else {
+                    finalDate = Date.parse(timeDate[2]+"-"+timeDate[1]+"-"+timeDate[0]+"T"+time);
+                }
+
+                var name = $(this).children('.nam').text();  // Get name
+                var url = 'http://kinozal.tv' + $(this).children('.nam').children().attr('href');
+
+                if (!name) return;
+
+                var moviesInQueue = 0;  // Wait until all IMDB rating were returned
+
+                // If Movie is not Russian
+                if (name.match(/\//g).length === 4) {
+                    name = name.split(' / ')[1];  // Get English title
+
+                    moviesInQueue += 1;
+                    getImdbRating(name, function(rating) {
+                        movies.push({
+                            'name': name,
+                            'time': finalDate,
+                            'url': url,
+                            'rating': rating,
+                            'seen': false
+                        });
+
+                        moviesInQueue -= 1;
+                        if (moviesInQueue === 0) callback(movies);
+                    });
+                } else {
+                    name = name.split(' / ')[0];  // Get Russian title
+
+                    movies.push({
+                        'name': name,
+                        'time': finalDate,
+                        'url': url,
+                        'rating': {score: 'N/A', votes: 'N/A'},
+                        'seen': false
+                    });
+                }
+            });
         });
-      });
-    });
+    }
 
-    movies.splice(0, 1); // First movie is empty, slicing out
-
-    callback(movies);
-  }
-
-  $.get('http://kinozal.tv/browse.php?s=&g=0&c=0&v=0&d=0&w=0&t=1&f=0/', parse_trs);
+    function getImdbRating(movieName, callback) {
+        $.getJSON("http://www.omdbapi.com/?i=&t=" + movieName, function(data) {
+            if (data['Response'] === 'False') {
+                callback({
+                    score: 'N/A',
+                    votes: 'N/A'
+                });
+            } else {
+                callback({
+                    score: data.imdbRating,
+                    votes: data.imdbVotes
+                });
+            }
+        });
+    }
 }
