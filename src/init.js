4bb4aacd4a04b259342a81d704c2b29bb2dc7cfd
Artyom Pertsovsky <artyom@pertsovsky.com>
2014-09-20 18:45:11 +0300

    Delete init.js

--- src/init.js
+++
@@ -1,7 +0,0 @@
-function initStorage() {
-  //chrome.storage.local.clear();
-  time_now = new Date().getTime();
-  //time_now = new Date('2014-04-01').getTime();
-	chrome.storage.local.set({'timeJoined': time_now});
-  chrome.storage.local.set({'movies': []});
-}
