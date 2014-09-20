f1168db745e14eff1da10a6aada9bc80371f8418
Artyom Pertsovsky <artyom@pertsovsky.com>
2014-09-20 18:45:00 +0300

    Move updateCounter to background, update parser as soon as chrome launched

--- src/background.js
+++ src/background.js
@@ -1,13 +1,44 @@
 var DELAY = 5;
 
 chrome.alarms.onAlarm.addListener(onAlarm);
-chrome.runtime.onInstalled.addListener(initStorage);
-chrome.runtime.onStartup.addListener(update);
 chrome.alarms.create('update', {'periodInMinutes': DELAY});
+
+chrome.runtime.onInstalled.addListener(initStorage);
+
+Parser.update();
 
 function onAlarm(alarm) {
   if (alarm['name'] == 'update') {
-    update();
+    Parser.update();
     chrome.alarms.create('update', {'periodInMinutes': DELAY});
   }
+}
+
+function updateCounter() {
+    var counter = 0;
+
+    chrome.storage.local.get('movies', function(storedMovies) {
+        storedMovies['movies'].forEach(function(movie) {
+            if (!movie['seen']) {
+                counter += 1;
+            }
+        });
+
+        counter = counter.toString();
+        if (counter === '0') {
+            chrome.browserAction.setBadgeText({text: ''});
+            window.close();
+        } else {
+            chrome.browserAction.setBadgeText({text: counter});
+        }
+    });
+}
+
+function initStorage() {
+    //chrome.storage.local.clear();
+    //time_now = new Date('2014-04-01').getTime();
+
+    var time_now = new Date().getTime();
+    chrome.storage.local.set({'timeJoined': time_now});
+    chrome.storage.local.set({'movies': []});
 }
